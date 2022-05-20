16a742c65604e2738b19a8b31434b376ab1f4816
Alisson Omena <omena.cco@gmail.com>
2022-05-20 10:00:57 -0300

    Food UI

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# ionic-food-ui
+# Ionic Food UI
+
+Esta é uma aplicação onde consumimos dados fake e os exibimos usando um design igual a aplicativos de delivery.
+
+## Executar a aplicação.
+
+Para executar a aplicação, execute o seguinte comando no terminal:
+
+```sh
+ionic serve
+```
+
+## Ferramentas para o tutorial:
+
+```sh
+npm i @ionic/cli -g
+```
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  categories = [];
+  highlights = [];
+  featured = [];
+
+  catSlideOpts = {
+    freeMode: true,
+    slidesPerView: 3.5,
+    slidesOffsetBefore: 11,
+    spaceBetween: 10
+  };
+
+  highlightSlideOpts =  {
+    slidesPerView: 1.05,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true
+  };
+
+  featuredSlideOpts = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    freeMode: true
+  };
+
+  showLocationDetail = false;
+
+  constructor(
+    private http: HttpClient
+  ) {}
+  
+  ngOnInit(): void {
+    this.http.get(
+      'https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json'
+    ).subscribe((res: any) => {
+      this.categories = res.categories;
+      this.highlights = res.highlights;
+      this.featured = res.featured;
+    })
+  }
+
+  doRefresh(ev: any) {
+    setTimeout(() => {
+      ev.target.complete();
+    }, 2000);
+  }
+
+  onScroll(ev) {
+    const offset = ev.detail.scrollTop;
+    this.showLocationDetail = offset > 40;
+  }
+}
